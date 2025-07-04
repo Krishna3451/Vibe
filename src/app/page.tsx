fe94df8e739c82d2de5f4488214cb3302fba5cf8
krishna3451 <fkrishna1729@gmail.com>
2025-07-04 18:51:31 +0530

    prisma & tRPC setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-export default function Home() {
-  return (
-    <div className="flex justify-center mt-100" >
+// import prisma from '@/lib/db'
 
-      <Button variant="destructive">
-        Hi there
-      </Button>
-    </div>
+'use client'
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { getQueryClient, trpc} from "@/trpc/server";
+import { Client } from "./client";
+import { Suspense, use } from "react";
+
+const Page = async ()=>{
+  const trpc = useTRPC();
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text : "Antonio "}))
+  
+
+   
+
+  return(
+    <HydrationBoundary state = {dehydrate(queryClient)}>
+      <Suspense fallback={<p>loading...</p>}>
+
+       <Client/>
+      </Suspense>
+
+    </HydrationBoundary>
   )
+
 }
+
+export default Page
