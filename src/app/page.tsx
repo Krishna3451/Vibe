dfaa80ecccc086ac478fd3fbeb85b538aa139870
krishna3451 <fkrishna1729@gmail.com>
2025-07-05 17:52:57 +0530

    03: Background Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,27 @@
-// import prisma from '@/lib/db'
-
 'use client'
+
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getQueryClient, trpc} from "@/trpc/server";
-import { Client } from "./client";
-import { Suspense, use } from "react";
+import { toast } from "sonner";
+import { useMutation } from "@tanstack/react-query";
 
-const Page = async ()=>{
+const Page =  () => {
   const trpc = useTRPC();
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text : "Antonio "}))
-  
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background Job started");
+      },
+    })
+  );
 
-   
+  return (
+    <div className='p-4 max-w-7xl mx-auto'>
+      <Button onClick={() => invoke.mutate({ text: "John" })}>
+        Invoke Background Job
+      </Button>
+    </div>
+  );
+};
 
-  return(
-    <HydrationBoundary state = {dehydrate(queryClient)}>
-      <Suspense fallback={<p>loading...</p>}>
-
-       <Client/>
-      </Suspense>
-
-    </HydrationBoundary>
-  )
-
-}
-
-export default Page
+export default Page;
